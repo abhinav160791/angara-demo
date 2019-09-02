e4989b2e05122204b560264f93b0ebc6f5dd4a57
Abhinav Bharti <abhinav.bharti@voylla.com>
2019-09-02 10:01:33 +0530

    built for deploy

---
+++ build/precache-manifest.69eefcce01336356d25de505ad0d3b46.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c6b78f4906202ba0573952bb9c8aacc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dde0de6974adac4995a4",
+    "url": "/static/css/main.ea65cbb5.chunk.css"
+  },
+  {
+    "revision": "5390e885b143f928456c",
+    "url": "/static/js/2.cc1fa82c.chunk.js"
+  },
+  {
+    "revision": "dde0de6974adac4995a4",
+    "url": "/static/js/main.63dceb38.chunk.js"
+  },
+  {
+    "revision": "86af7a3bccbd0f6b8972",
+    "url": "/static/js/runtime~main.e8b26d8e.js"
+  }
+]);
